c7528464f0825199afeceea7ab7650ab8569f05e
Matt Anderson <matty_ander@hotmail.com>
2019-01-15 21:51:37 -0500

    Updates

---
+++ precache-manifest.0aff78af2ad61dad3a813c16959ad914.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "259905c6ce038bc609fa",
+    "url": "/msft-frontend-capstone/static/js/runtime~main.259905c6.js"
+  },
+  {
+    "revision": "621cc04169099c007182",
+    "url": "/msft-frontend-capstone/static/js/main.621cc041.chunk.js"
+  },
+  {
+    "revision": "b387dc35c969a952b4ea",
+    "url": "/msft-frontend-capstone/static/js/1.b387dc35.chunk.js"
+  },
+  {
+    "revision": "621cc04169099c007182",
+    "url": "/msft-frontend-capstone/static/css/main.cadc0499.chunk.css"
+  },
+  {
+    "revision": "b387dc35c969a952b4ea",
+    "url": "/msft-frontend-capstone/static/css/1.fac72d81.chunk.css"
+  },
+  {
+    "revision": "08dc2261593d511dffc157b0f0e29322",
+    "url": "/msft-frontend-capstone/index.html"
+  }
+];
