ccd15263092e77a3d1714fe05d57deb73209349e
Matt Anderson <matt.anderson@mississauga.ca>
2019-01-22 13:20:01 -0500

    started about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,11 @@ const About = () => {
   return (
     <section>
       <h1>About</h1>
-
+      {/* Image credit: Photo by rawpixel on Unsplash */}
+      <div className="about-banner container-fluid">
+        <div className="inner-text">Your Groceries
+      <br /><span class="line-2">Reimagined</span></div>
+      </div>
     </section>
   );
 }
