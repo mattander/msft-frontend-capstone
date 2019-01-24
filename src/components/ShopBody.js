95ffae472531af745b4d3c390db6429573f5f5dd
Matt Anderson <matty_ander@hotmail.com>
2019-01-24 00:03:09 -0500

    bug squashed, code refactored, looks great

--- src/components/ShopBody.js
+++ src/components/ShopBody.js
@@ -35,114 +35,35 @@ class ShopBody extends Component {
           return <Redirect to="/404" />
         } else {
           //valid category
-          const itemsList = this.props.data.filter(item => toTitleCase(item.category, ' ') === this.props.category)[0].subcategories.filter(item => item.name === this.props.subcategory)[0].items;
 
-          const filterItems = (items) => {
-            let filteredList = items;
-            if (this.props.filters.sortBy) {
-              if (this.props.filters.sortBy === 'lowToHigh') {
-                const listCopy = filteredList.sort((a, b) => a.price - b.price);
-                filteredList = listCopy;
-              } else if (this.props.filters.sortBy === 'highToLow') {
-                const listCopy = filteredList.sort((a, b) => b.price - a.price);
-                filteredList = listCopy;
-              } else if (this.props.filters.sortBy === 'aToZ') {
-                const listCopy = filteredList.sort((a, b) => {
-                  if (a.name.toString() < b.name.toString()) {
-                    return -1;
-                  } else {
-                    return 1;
-                  }
-                });
-                filteredList = listCopy;
-              } else if (this.props.filters.sortBy === 'zToA') {
-                const listCopy = filteredList.sort((a, b) => {
-                  if (a.name.toString() > b.name.toString()) {
-                    return -1;
-                  } else {
-                    return 1;
-                  }
-                });
-                filteredList = listCopy;
-              } else if (this.props.filters.sortBy === 'ratingHighToLow') {
-                const listCopy = filteredList.sort((a, b) => {
-                  if (parseInt(b.rating) <= parseInt(a.rating)) {
-                    return -1;
-                  } else {
-                    return 1;
-                  }
-                });
-                filteredList = listCopy;
-              } else if (this.props.filters.sortBy === 'ratingLowToHigh') {
-                const listCopy = filteredList.sort((a, b) => {
-                  if (parseInt(a.rating) <= parseInt(b.rating)) {
-                    return -1;
-                  } else {
-                    return 1;
-                  }
-                });
-                filteredList = listCopy;
-              } else {
-                filteredList = items;
-              }
-            }
-
-            if (this.props.filters.inStockOnly) {
-              const listCopy = filteredList.filter(item => item.stock > 0);
-              filteredList = listCopy;
-            }
-            return filteredList;
-          };
-
-          const filteredItems = filterItems(itemsList);
-
-          // Pagination code. Saving this just in case.
-
-          const paginateItems = (list, itemsPerPage) => {
-            if (list.length > itemsPerPage) {
-              let paginatedItemsList = [];
-              if (parseInt(list.length) % parseInt(itemsPerPage > 0)) {
-                for (let i = 0; i <= parseInt(list.length / itemsPerPage); i++) {
-                  const startIndex = itemsPerPage * i;
-                  const stopIndex = i < parseInt(list.length / itemsPerPage) ? itemsPerPage * (i + 1) : list.length;
-                  paginatedItemsList.push(list.slice(startIndex, stopIndex));
-                }
-              } else {
-                for (let i = 0; i < parseInt(list.length / itemsPerPage); i++) {
-                  const startIndex = itemsPerPage * i;
-                  const stopIndex = i < parseInt(list.length / itemsPerPage) ? itemsPerPage * (i + 1) : list.length;
-                  paginatedItemsList.push(list.slice(startIndex, stopIndex));
-                }
-              }
-              return paginatedItemsList;
-            } else {
-              return list;
-            }
-          }
 
           let paginatedItems = [];
 
-          if (filteredItems.length > this.props.filters.showNumItems) {
-            paginatedItems = paginateItems(filteredItems, this.props.filters.showNumItems).map((item, index) => {
-              return item.map((item, index) => {
+          if (this.props.itemsList.paged) {
+            if (this.state.page > this.props.itemsList.list.length - 1) {
+              // if you change the number shown while on a page that will no longer exist
+              // update the page to the furthest possible
+              this.setState({ page: this.props.itemsList.list.length - 1 });
+            } else {
+              paginatedItems = this.props.itemsList.list[this.state.page].map((item, index) => {
                 return (
                   <Card onCartAddItem={this.props.onCartAddItem} key={'product-' + item.name} itemData={item} itemIndex={index} itemType="product" categoryInfo={this.props.categoryInfo} history={this.props.history} />
-                )
-              })
-            });
+                );
+              });
+            }
           } else {
-            paginatedItems = filteredItems.map((item, index) => {
+            paginatedItems = this.props.itemsList.list.map((item, index) => {
               return (
                 <Card onCartAddItem={this.props.onCartAddItem} key={'product-' + item.name} itemData={item} itemIndex={index} itemType="product" categoryInfo={this.props.categoryInfo} history={this.props.history} />
               );
             })
           };
 
-          const paginationButtons = paginateItems(filteredItems, this.props.filters.showNumItems).map((item, index) => {
+          const pageButtons = this.props.itemsList.list.map((item, index) => {
             return <li key={'page' + index} className={this.state.page === index ? "page-item active test" : "page-item no"}><button data-page={index} onClick={(e) => this.goToPage(e)} className="page-link">{index + 1}</button></li>
-          })
+          });
 
-          if (filteredItems.length === 0) {
+          if (this.props.itemsList.list.length === 0) {
             return (
               <section id="shopBodyContent" className="p-5">
                 <p className="lead">Sorry, it looks like we don't have any products in this category right now.</p>
@@ -155,15 +76,15 @@ class ShopBody extends Component {
                 {/* rubric14 - The user shall see a controls bar */}
                 {/* rubric20 - The user shall see a grid that is populated with the products of
 the selected category  */}
-                <FilterBarContainer itemsShown={paginatedItems} page={this.state} itemsList={itemsList} filteredItemsList={filteredItems} categoryInfo={this.props.categoryInfo} />
+                <FilterBarContainer itemsShown={paginatedItems.length} page={this.state} itemsList={this.props.filteredItems} filteredItemsList={this.props.itemsList.list} categoryInfo={this.props.categoryInfo} />
                 <div className="card-container">
-                  {filteredItems.length > this.props.filters.showNumItems ? paginatedItems[this.state.page] : paginatedItems}
+                  {paginatedItems}
                 </div>
-                {filteredItems.length > parseInt(this.props.filters.showNumItems) ? <nav className="d-flex justify-content-center">
+                {this.props.itemsList.paged ? <nav className="d-flex justify-content-center">
                   <ul className="pagination">
                     {this.state.page === 0 ? <li className="page-item disabled"><button className="page-link" onClick={(e) => this.prevPage(e)}><FontAwesomeIcon icon="chevron-left" /> Prev</button></li> : <li className="page-item"><button className="page-link" onClick={(e) => this.prevPage(e)}><FontAwesomeIcon icon="chevron-left" /> Prev</button></li>}
-                    {paginationButtons}
-                    {this.state.page === paginateItems(filteredItems, this.props.filters.showNumItems).length - 1 ? <li className="page-item disabled"><button className="page-link" onClick={(e) => this.nextPage(e)}>Next <FontAwesomeIcon icon="chevron-right" /></button></li> : <li className="page-item"><button className="page-link" onClick={(e) => this.nextPage(e)}>Next <FontAwesomeIcon icon="chevron-right" /></button></li>}
+                    {pageButtons}
+                    {this.state.page === this.props.itemsList.list.length - 1 ? <li className="page-item disabled"><button className="page-link" onClick={(e) => this.nextPage(e)}>Next <FontAwesomeIcon icon="chevron-right" /></button></li> : <li className="page-item"><button className="page-link" onClick={(e) => this.nextPage(e)}>Next <FontAwesomeIcon icon="chevron-right" /></button></li>}
                   </ul>
                 </nav> : null}
               </section>
--- src/containers/ShopBodyContainer.js
+++ src/containers/ShopBodyContainer.js
@@ -4,6 +4,87 @@ import cartAddItem from '../actions/cartAddItem';
 import { toTitleCase } from '../constants/constants';
 
 const mapStateToProps = (state, ownProps) => {
+  const filterItems = (items) => {
+    let filteredList = items;
+    if (state.filters.sortBy) {
+      if (state.filters.sortBy === 'lowToHigh') {
+        const listCopy = filteredList.sort((a, b) => a.price - b.price);
+        filteredList = listCopy;
+      } else if (state.filters.sortBy === 'highToLow') {
+        const listCopy = filteredList.sort((a, b) => b.price - a.price);
+        filteredList = listCopy;
+      } else if (state.filters.sortBy === 'aToZ') {
+        const listCopy = filteredList.sort((a, b) => {
+          if (a.name.toString() < b.name.toString()) {
+            return -1;
+          } else {
+            return 1;
+          }
+        });
+        filteredList = listCopy;
+      } else if (state.filters.sortBy === 'zToA') {
+        const listCopy = filteredList.sort((a, b) => {
+          if (a.name.toString() > b.name.toString()) {
+            return -1;
+          } else {
+            return 1;
+          }
+        });
+        filteredList = listCopy;
+      } else if (state.filters.sortBy === 'ratingHighToLow') {
+        const listCopy = filteredList.sort((a, b) => {
+          if (parseInt(b.rating) <= parseInt(a.rating)) {
+            return -1;
+          } else {
+            return 1;
+          }
+        });
+        filteredList = listCopy;
+      } else if (state.filters.sortBy === 'ratingLowToHigh') {
+        const listCopy = filteredList.sort((a, b) => {
+          if (parseInt(a.rating) <= parseInt(b.rating)) {
+            return -1;
+          } else {
+            return 1;
+          }
+        });
+        filteredList = listCopy;
+      } else {
+        filteredList = items;
+      }
+    }
+
+    if (state.filters.inStockOnly) {
+      const listCopy = filteredList.filter(item => item.stock > 0);
+      filteredList = listCopy;
+    }
+    return filteredList;
+  };
+
+  const paginate = (list, num) => {
+    if (list.length > num) {
+      let listOfItems = [];
+      if (list.length % num === 0) {
+        // list length divided by number per page is 0, perfect divide so subtract one from
+        // the for loop limit
+        for (let i = 0; i <= parseInt(list.length / num) - 1; i++) {
+          const startIndex = num * i;
+          const stopIndex = i <= parseInt(list.length / num) - 1 ? num * (i + 1) : list.length;
+          listOfItems.push(list.slice(startIndex, stopIndex));
+        }
+      } else {
+        // list length divided by number per page leaves a remainder
+        for (let i = 0; i <= parseInt(list.length / num); i++) {
+          const startIndex = num * i;
+          const stopIndex = i <= parseInt(list.length / num) ? num * (i + 1) : list.length;
+          listOfItems.push(list.slice(startIndex, stopIndex));
+        }
+      }
+      return { list: listOfItems, paged: true };
+    } else {
+      return { list: list, paged: false };
+    }
+  }
 
   return {
     data: state.shopData[0],
@@ -12,8 +93,10 @@ const mapStateToProps = (state, ownProps) => {
     filters: state.filters,
     categoryInfo: ownProps.categoryInfo,
     history: ownProps.history,
-    category: ownProps.categoryInfo.category ? toTitleCase(ownProps.categoryInfo.category, ' ', '-') : null,
-    subcategory: ownProps.categoryInfo.subcategory ? toTitleCase(ownProps.categoryInfo.subcategory, ' ', '-') : null
+    category: ownProps.categoryInfo.category ? toTitleCase(ownProps.categoryInfo.category) : null,
+    subcategory: ownProps.categoryInfo.subcategory ? toTitleCase(ownProps.categoryInfo.subcategory) : null,
+    filteredItems: ownProps.categoryInfo.subcategory && state.loaded ? (filterItems(state.shopData[0].filter(item => toTitleCase(item.category, ' ') === toTitleCase(ownProps.categoryInfo.category))[0].subcategories.filter(item => item.name === toTitleCase(ownProps.categoryInfo.subcategory))[0].items)) : null,
+    itemsList: ownProps.categoryInfo.subcategory && state.loaded ? paginate(filterItems(state.shopData[0].filter(item => toTitleCase(item.category, ' ') === toTitleCase(ownProps.categoryInfo.category))[0].subcategories.filter(item => item.name === toTitleCase(ownProps.categoryInfo.subcategory))[0].items), state.filters.showNumItems) : null
   };
 };
 
